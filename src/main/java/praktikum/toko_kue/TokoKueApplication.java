70545116795a1d44059e0c655199abff2db0a1bf
febriyantiazahra <febriyantiazhraa@gmail.com>
2022-10-18 15:25:16 +0700

    menambahkan database

---
+++ src/main/java/praktikum/toko_kue/TokoKueApplication.java
@@ -0,0 +1,13 @@
+package praktikum.toko_kue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoKueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoKueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/praktikum/toko_kue/TokoKueApplicationTests.java
@@ -0,0 +1,13 @@
+package praktikum.toko_kue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoKueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
